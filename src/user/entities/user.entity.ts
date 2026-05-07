ac4b06bdb7dc57735bb44f6c7f48be140294b72a
chanweiyan <chanweiyan007@gmail.com>
2026-05-07 12:08:19 +0800

    fix: add user & permission entities

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, UpdateDateColumn } from "typeorm";
+import { PrimaryGeneratedColumn } from "typeorm/browser";
+
+@Entity({
+  comment: '用户表',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+    comment: '用户名',
+  })
+  username: string;
+
+  @Column({
+    length: 50,
+    comment: '密码',
+  })
+  password: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    comment: '创建时间',
+  })
+  createTime: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    comment: '更新时间',
+  })
+  updateTime: Date;
+}
